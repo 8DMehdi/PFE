1770fbdc6dc0db5f9dd434a0228f9b797774462f
TheDocteurC <clegaudillat@gmail.com>
2025-03-10 17:23:04 +0100

    RecupBranchKhady

--- Assets/PFE/Scripts/Collectible.cs
+++ Assets/PFE/Scripts/Collectible.cs
@@ -1,38 +1,42 @@
 
 
-// using UnityEngine;
-
-// public class Collectible : MonoBehaviour
-// {
-//     private void OnTriggerEnter2D(Collider2D other)
-//     {
-//         var player = other.GetComponent<PlayerRespawn>(); // Vérifie si c'est un joueur
-//         if (player != null)
-//         {
-//             CollectibleManager.Instance.AddCollectible(5); // Ajoute 5 points
-//             Destroy(gameObject); // Supprime l'objet collecté
-//         }
-//         SoundManager.Instance.PlayCollectSound();
-//     }
-// }
-
-
 using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
-  private void OnTriggerEnter2D(Collider2D other)
-{
-
-    var player = other.GetComponent<PlayerFlight>();
-    if (player == null) player = other.GetComponentInParent<PlayerFlight>();
-
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Collectible ramassé ! Activation du vol...");
-        player.UnlockFlight(); // Appelle la méthode UnlockFlight pour activer l'aptitude
-        Destroy(gameObject);
+        var player = other.GetComponent<PlayerRespawn>(); // Vérifie si c'est un joueur
+        if (player != null)
+        {
+            CollectibleManager.Instance.AddCollectible(5); // Ajoute 5 points
+            Destroy(gameObject); // Supprime l'objet collecté
+        }
         SoundManager.Instance.PlayCollectSound();
     }
 }
 
-}
+
+// using UnityEngine;
+
+// public class Collectible : MonoBehaviour
+// {
+//   private void OnTriggerEnter2D(Collider2D other)
+// {
+
+//     var player = other.GetComponent<PlayerFlight>();
+//     if (player == null) player = other.GetComponentInParent<PlayerFlight>();
+
+//     {
+//         Debug.Log("Collectible ramassé ! Activation du vol...");
+//         player.UnlockFlight(); // Appelle la méthode UnlockFlight pour activer l'aptitude
+//         Destroy(gameObject);
+//         SoundManager.Instance.PlayCollectSound();
+//     }
+//      else
+//     {
+//         Debug.LogError("ERREUR : Aucun PlayerFlight trouvé sur l'objet qui a déclenché le trigger !");
+//     }
+// }
+
+// }
